5450abb0ecfa38fef3cb7c0772cd8c62584bbb43
martinrgt <martin.ringot@outlook.com>
2021-05-24 16:38:29 +0200

    textbox et okbutton + TutoText

--- src/TutoText.hx
+++ src/TutoText.hx
@@ -2,9 +2,11 @@ class TutoText
 {
     public static var texts : Map<Int, String> = 
     [
-        1 => "Phrase niveau 1",
-        2 => "Phrase niveau 2",
-        4 => "Phrase niveau 4",
-        5 => 'Hello I am <font color="#ac3232">red</font>'
+        1 => "Si je fléchis, il me faut de l’<font color="#4a6ca9">eau</font>.",
+        2 => "Pour m’aider à me déplacer, il faut de l’<font color="#baa42e">air</font>.",
+        3 => "La <font color="#baa42e">direction</font> du vent est entre vos mains.",
+        4 => 'La <font color="#597d33">terre</font> m’aidera à pousser plus vite.',
+        5 => 'Si je pousse mal, le <font color="#ad2727">feu</font> peut être une solution.',
+        8 => 'Avec le <font color="#ad2727">feu</font>, je fléchis plus vite.'
     ];
 }
