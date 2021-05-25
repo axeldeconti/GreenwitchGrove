f0d58391838f044d593fcb1515af6ae23c699cb9
martinrgt <martin.ringot@outlook.com>
2021-05-25 19:03:55 +0200

    correction textes

--- src/TutoText.hx
+++ src/TutoText.hx
@@ -2,11 +2,11 @@ class TutoText
 {
     public static var texts : Map<Int, String> = 
     [
-        1 => 'Si je fléchis, il me faut de l’<font color="#4a6ca9">eau</font>.',
-        2 => 'Pour m’aider à me déplacer, il faut de l’<font color="#baa42e">air</font>.',
-        3 => 'La <font color="#baa42e">direction</font> du vent est entre vos mains.',
-        4 => 'La <font color="#597d33">terre</font> m’aidera à pousser plus vite.',
-        5 => 'Si je pousse mal, le <font color="#ad2727">feu</font> peut être une solution.',
-        8 => 'Avec le <font color="#ad2727">feu</font>, je fléchis plus vite.'
+        1 => '<p>Parfois, il suffit de me laisser pousser.</p><p>Mais si je fléchis, il me faut de l’<font color="#4a6ca9">eau</font>.</p>',
+        2 => '<p>Je pousse naturellement vers le haut.</p><p>Mais je peux aller sur le côté</p><p>si on fait appel à l’<font color="#baa42e">air</font>.</p>',
+        3 => '<p> Si le vent ne souffle pas dans la bonne <font color="#baa42e">direction</font>,</p><p>il est possible de la <font color="#baa42e">changer</font> grâce au <font color="#baa42e">ruban</font>.</p>',
+        4 => '<p>Si vous n’avez pas le temps de me voir pousser à</p><p>mon rythme, alors la <font color="#597d33">terre</font> me fera accélérer.</p>',
+        5 => '<p>Parfois, je fais fausse route.</p><p>Le <font color="#ad2727">feu</font> peut être une solution.</p>',
+        8 => 'Avec le <font color="#ad2727">feu</font>, je fléchis aussi plus vite.'
     ];
 }
